1e28f8b47e088a96a94fed0f784cf7dbd528e3c2
John-C-Kuang <kuang.che@northeastern.edu>
2023-11-09 12:42:16 -0500

    kanbas server partial

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express"
 import cors from "cors";
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
 
+CourseRoutes(app);
 Lab5(app);
 Hello(app)
 
