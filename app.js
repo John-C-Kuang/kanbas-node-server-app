4ffb5dff9dca78e29b1db4956b7e2149576bf7a1
John-C-Kuang <kuang.che@northeastern.edu>
2023-11-11 18:44:37 -0500

    a5 extra credit

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import AssignmentsRoutes from "./assignments/routes.js";
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
 
+AssignmentsRoutes(app)
 ModuleRoutes(app)
 CourseRoutes(app);
 Lab5(app);
