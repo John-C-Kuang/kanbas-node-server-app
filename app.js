f11811dee612ae643d2b0167320eaf2997c37d0f
John-C-Kuang <kuang.che@northeastern.edu>
2023-11-30 10:48:42 -0500

    a6 initial

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from "./assignments/routes.js";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express()
 
@@ -25,5 +29,6 @@ ModuleRoutes(app)
 CourseRoutes(app);
 Lab5(app);
 Hello(app)
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
